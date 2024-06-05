f1956f7ed0ae3295fd61ceda8be0ca6090e5c178
Mara@lain <test@test.com>
2024-06-05 10:50:19 +0200

    schöner output :3

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
@@ -6,209 +7,121 @@
 #include "utils.h"
 
 #define MAX_DEP_COUNT 10
+#define MAX_STRING_W 100
 #define DEF_LANG "de"
 //other: en
 
-
-int main(int c, char** v){
-
-    if(c == 1) {
-        char* stationname = escapeString("Gera Hbf");
-        Station* station = getStation(stationname);
-        int dcount;
-        Departure** det = getDepartures(station, &dcount, MAX_DEP_COUNT, DEF_LANG);
-
-	    if(det == NULL){
-		    printf("no departures could be loaded. aborting.\n");
-		    return -1;
-	    }
-	    printf("Abfahrten für %s:\n", station->name);
-            for(int i = 0; i < dcount; i++) {
-                if(det[i] == NULL) {
-                    printf("ja nun\n");
-                    continue;
-                }
-                int rcount = det[i]->rcount;
-                bool cancelled = false;
-                bool delayed = false;
-                bool changedP = false;
-
-                //printf("%s -> %s, %s (%s) - %s (%s)\n", det[i]->line->name, det[i]->direction, det[i]->tPlannedWhen, det[i]->tWhen, det[i]->plannedplatform, det[i]->platform);
-
-                for(int y = 0; y < rcount; y++) {
-                    if(!(strcmp(det[i]->remarks[y]->code, "cancelled"))) {
-                        cancelled = true;
-                        break;
-                    }
-                }
-                if((strcmp(det[i]->plannedplatform, det[i]->platform))) {
-                    changedP = true;
-                }
-                if(det[i]->delay/60) {
-                    delayed = true;
-                }
-
-                if(cancelled) {
-                    printf(" - %s (%s) nach (%s) um (%s) fällt heute aus.\n", det[i]->line->name, det[i]->line->fahrtNr, det[i]->direction, det[i]->tPlannedWhen);
-                    for(int y = 0; y < det[i]->rcount; y++) {
-                        /*if(det[i]->remarks[y] != NULL) {
-                            printf("\tHinweis: (type)%s; (code)%s; (text)%s\n", det[i]->remarks[y]->type, det[i]->remarks[y]->code, det[i]->remarks[y]->text);
-                        }
-                        printf("hinweis nr: %d\n", y);*/
-                        if(!((det[i]->remarks[y] == NULL) || (det[i]->remarks[y]->text == NULL || !(strcmp(det[i]->remarks[y]->code, "cancelled"))))) {
-                            printf("\tHinweis zu %s: %s (%s)\n", det[i]->remarks[y]->type, det[i]->remarks[y]->text, det[i]->remarks[y]->code);
-                        }
-                        /*if(det[i]->remarks[y] != NULL && det[i]->remarks[y]->type != NULL) {
-                            printf("\tHinweis: %s\n", det[i]->remarks[y]->type);
-                        }
-                        if(det[i]->remarks[y] != NULL && det[i]->remarks[y]->code != NULL) {
-                            printf("\tHinweis: %s\n", det[i]->remarks[y]->code);
-                        }
-                        if(det[i]->remarks[y] != NULL && det[i]->remarks[y]->text != NULL) {
-                            printf("\tHinweis: %s\n", det[i]->remarks[y]->text);
-                        }*/
-                    }
-
-                    continue;
-                }
-                printf(" - %s (%s) nach %s ", det[i]->line->name, det[i]->line->fahrtNr, det[i]->direction);
-                if(delayed) {
-                    printf("heute um %s (ursprünglich %s), ca. %d Minuten verspätet ", det[i]->tWhen, det[i]->tPlannedWhen, det[i]->delay/60);
-                } else {
-                    printf("heute um %s ", det[i]->tPlannedWhen);
-                }
-                if(changedP) {
-                    printf("abweichend auf Gleis %s.\n", det[i]->platform);
-                } else {
-                    printf("auf Gleis %s\n", det[i]->platform);
-                }
-
-                //printf("Hinweise für %s: %d\n", det[i]->line->name, det[i]->rcount);
-                for(int y = 0; y < det[i]->rcount; y++) {
-                    /*if(det[i]->remarks[y] != NULL) {
-                        printf("\tHinweis: (type)%s; (code)%s; (text)%s\n", det[i]->remarks[y]->type, det[i]->remarks[y]->code, det[i]->remarks[y]->text);
-                    }
-                    printf("hinweis nr: %d\n", y);*/
-                    /*if(!((det[i]->remarks[y] == NULL) || (det[i]->remarks[y]->text == NULL || !(strcmp(det[i]->remarks[y]->code, "cancelled"))))) {
-                        printf("\tHinweis: %s\n", det[i]->remarks[y]->text);
-                    }*/
-                    /*if(det[i]->remarks[y] != NULL && det[i]->remarks[y]->type != NULL) {
-                        printf("\tHinweis: %s\n", det[i]->remarks[y]->type);
-                    }
-                    if(det[i]->remarks[y] != NULL && det[i]->remarks[y]->code != NULL) {
-                        printf("\tHinweis: %s\n", det[i]->remarks[y]->code);
-                    }
-                    if(det[i]->remarks[y] != NULL && det[i]->remarks[y]->text != NULL) {
-                        printf("\tHinweis: %s\n", det[i]->remarks[y]->text);
-                    }*/
-                    if(!((det[i]->remarks[y] == NULL) || (det[i]->remarks[y]->text == NULL || !(strcmp(det[i]->remarks[y]->code, "cancelled"))))) {
-                        printf("\tHinweis zu %s: %s (%s)\n", det[i]->remarks[y]->type, det[i]->remarks[y]->text, det[i]->remarks[y]->code);
-                    }
-                }
-
-            }
-            for(int y = 0; y < dcount; y++) {
-                freeDeparture(det[y], det[y]->rcount);
-            }
-        free(det);
-        free(station);
-
-        return 0;
+int printWString(char* s) {
+    bool shouldWrap = false;
+    for(int c = 0; *s != 0; s++, c++) {
+        if(*s == '\n') {
+            continue;
+        }
+        if(c == MAX_STRING_W) {
+            shouldWrap = true;
+            c = 0;
+        }
+        if(shouldWrap && isspace(*s)) {
+            putc('\n', stdout);
+            putc('\t', stdout);
+            putc('\t', stdout);
+            shouldWrap = false;
+        } else {
+            putc(*s, stdout);
+        }
     }
-
-    for(int j = 1; j < c; j++){
-        char* stationname = escapeString(v[j]);
-        //printf("unescaped: %s; escaped: %s\n", v[j], stationname);
-        Station* station = getStation(stationname);
-        //printf("bahnhof %d: %s (%s)\n", j, station->name, station->id);
-
-        int dcount;
-        Departure** det = getDepartures(station, &dcount, MAX_DEP_COUNT, DEF_LANG);
-	
-	    if(det == NULL){
-		    printf("no departures could be loaded. aborting.\n");
-		    return -1;
-	    }
-	    printf("Abfahrten für %s:\n", station->name);
-            for(int i = 0; i < dcount; i++) {
-                if(det[i] == NULL) {
-                    printf("ja nun\n");
-                    continue;
-                }
-                int rcount = det[i]->rcount;
-                bool cancelled = false;
-                bool delayed = false;
-                bool changedP = false;
-
-                //printf("%s -> %s, %s (%s) - %s (%s)\n", det[i]->line->name, det[i]->direction, det[i]->tPlannedWhen, det[i]->tWhen, det[i]->plannedplatform, det[i]->platform);
-
-                for(int y = 0; y < rcount; y++) {
-                    if(!(strcmp(det[i]->remarks[y]->code, "cancelled"))) {
-                        cancelled = true;
-                        break;
-                    }
-                }
-                if((strcmp(det[i]->plannedplatform, det[i]->platform))) {
-                    changedP = true;
-                }
-                if(det[i]->delay/60) {
-                    delayed = true;
-                }
-
-                if(cancelled) {
-                    printf(" - %s (%s) nach (%s) um (%s) fällt heute aus.\n", det[i]->line->name, det[i]->line->fahrtNr, det[i]->direction, det[i]->tPlannedWhen);
-                    for(int y = 0; y < det[i]->rcount; y++) {
-                        if(!((det[i]->remarks[y] == NULL) || (det[i]->remarks[y]->text == NULL || !(strcmp(det[i]->remarks[y]->code, "cancelled"))))) {
-                            printf("\tHinweis zu %s: %s (%s)\n", det[i]->remarks[y]->type, det[i]->remarks[y]->text, det[i]->remarks[y]->code);
-                        }
-                    }
-                    continue;
-                }
-                printf(" - %s (%s) nach %s ", det[i]->line->name, det[i]->line->fahrtNr, det[i]->direction);
-                if(delayed) {
-                    printf("heute um %s (ursprünglich %s), ca. %d Minuten verspätet ", det[i]->tWhen, det[i]->tPlannedWhen, det[i]->delay/60);
-                } else {
-                    printf("heute um %s ", det[i]->tPlannedWhen);
-                }
-                if(changedP) {
-                    printf("abweichend auf Gleis %s.\n", det[i]->platform);
-                } else {
-                    printf("auf Gleis %s\n", det[i]->platform);
-                }
-
-                //printf("Hinweise für %s: %d\n", det[i]->line->name, det[i]->rcount);
-                for(int y = 0; y < det[i]->rcount; y++) {
-                    /*if(det[i]->remarks[y] != NULL) {
-                        printf("\tHinweis: (type)%s; (code)%s; (text)%s\n", det[i]->remarks[y]->type, det[i]->remarks[y]->code, det[i]->remarks[y]->text);
-                    }
-                    printf("hinweis nr: %d\n", y);
-                    /*if(!((det[i]->remarks[y] == NULL) || (det[i]->remarks[y]->text == NULL || !(strcmp(det[i]->remarks[y]->code, "cancelled"))))) {
-                        printf("\tHinweis: %s\n", det[i]->remarks[y]->text);
-                    }*/
-                    /*if(det[i]->remarks[y] != NULL && det[i]->remarks[y]->type != NULL) {
-                        printf("\tHinweis: %s\n", det[i]->remarks[y]->type);
-                    }
-                    if(det[i]->remarks[y] != NULL && det[i]->remarks[y]->code != NULL) {
-                        printf("\tHinweis: %s\n", det[i]->remarks[y]->code);
-                    }
-                    if(det[i]->remarks[y] != NULL && det[i]->remarks[y]->text != NULL) {
-                        printf("\tHinweis: %s\n", det[i]->remarks[y]->text);
-                    }*/
-                    if(!((det[i]->remarks[y] == NULL) || (det[i]->remarks[y]->text == NULL || !(strcmp(det[i]->remarks[y]->code, "cancelled"))))) {
-                        printf("\tHinweis zu %s: %s (%s)\n", det[i]->remarks[y]->type, det[i]->remarks[y]->text, det[i]->remarks[y]->code);
-                    }
-                }
-
-            }
-            for(int y = 0; y < dcount; y++) {
-                freeDeparture(det[y], det[y]->rcount);
-            }
-        free(det);
-        free(stationname);
-        free(station);
-    }
-
-
 
     return 0;
+}
+
+int printDepartures(const char* sname) {
+    const char* stationname = escapeString(sname);
+    Station* station = getStation(stationname);
+    int dcount;
+    Departure** det = getDepartures(station, &dcount, MAX_DEP_COUNT, DEF_LANG);
+
+    if(det == NULL){
+        printf("no departures could be loaded. aborting.\n");
+        return 1;
+    }
+    printf("Abfahrten für %s:\n", station->name);
+    for(int i = 0; i < dcount; i++) {
+        if(det[i] == NULL) {
+            printf("ja nun\n");
+            continue;
+        }
+        const int rcount = det[i]->rcount;
+        bool cancelled = false;
+        bool delayed = false;
+        bool changedP = false;
+
+        for(int y = 0; y < rcount; y++) {
+            if(!(strcmp(det[i]->remarks[y]->code, "cancelled"))) {
+                cancelled = true;
+                break;
+            }
+        }
+        if((strcmp(det[i]->plannedplatform, det[i]->platform))) {
+            changedP = true;
+        }
+        if(det[i]->delay/60) {
+            delayed = true;
+        }
+
+        if(cancelled) {
+            printf(" - %s (%s) nach (%s) um (%s) fällt heute aus.\n", det[i]->line->name, det[i]->line->fahrtNr, det[i]->direction, det[i]->tPlannedWhen);
+            for(int y = 0; y < det[i]->rcount; y++) {
+                if(!((det[i]->remarks[y] == NULL) || (det[i]->remarks[y]->text == NULL || !(strcmp(det[i]->remarks[y]->code, "cancelled"))))) {
+                    printf("\t - Hinweis bezüglich %s:\n\t\t", det[i]->remarks[y]->type);
+                    char* s = det[i]->remarks[y]->text;
+                    printWString(s);
+                    printf(" (%s)\n", det[i]->remarks[y]->code);
+                }
+            }
+            continue;
+        }
+
+        printf(" - %s (%s) nach %s ", det[i]->line->name, det[i]->line->fahrtNr, det[i]->direction);
+        if(delayed) {
+            printf("heute um %s (ursprünglich %s), ca. %d Minuten verspätet ", det[i]->tWhen, det[i]->tPlannedWhen, det[i]->delay/60);
+        } else {
+            printf("heute um %s ", det[i]->tPlannedWhen);
+        }
+        if(changedP) {
+            printf("abweichend auf Gleis %s.\n", det[i]->platform);
+        } else {
+            printf("auf Gleis %s\n", det[i]->platform);
+        }
+
+        for(int y = 0; y < det[i]->rcount; y++) {
+            if(!((det[i]->remarks[y] == NULL) || (det[i]->remarks[y]->text == NULL || !(strcmp(det[i]->remarks[y]->code, "cancelled"))))) {
+                printf("\t - Hinweis bezüglich %s:\n\t\t", det[i]->remarks[y]->type);
+                char* s = det[i]->remarks[y]->text;
+                printWString(s);
+                printf(" (%s)\n", det[i]->remarks[y]->code);
+            }
+        }
+    }
+    for(int y = 0; y < dcount; y++) {
+        freeDeparture(det[y], det[y]->rcount);
+    }
+    free(det);
+    free(station);
+
+    return 0;
+}
+
+
+int main(const int c, char** v){
+    int ret = 0;
+
+    if(c == 1) {
+        ret = printDepartures("Gera Hbf");
+    } else {
+        for(int j = 1; j < c; j++){
+            ret = printDepartures(v[j]);
+        }
+    }
+
+    return ret;
 }
